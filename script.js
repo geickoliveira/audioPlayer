bf6024317d527983daa39c533b30338de13d6f08
Geickson Oliveira <geickson2@gmail.com>
2022-02-02 00:55:50 +0000

    Designer update

---
+++ script.js
@@ -0,0 +1,23 @@
+let music = document.getElementById("music");
+let music01 = document.getElementById("music01");
+let music02 = document.getElementById("music02");
+let music03 = document.getElementById("music03");
+
+function play(){
+    music.play(music03);
+}
+function pause(){
+    music.pause(music03);
+}
+function back(){
+
+}
+function next(){
+
+}
+function stop(){
+
+}
+function volume(){
+    music.volume= 0.5;
+}
